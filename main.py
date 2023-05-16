05869a2864c4987f5c738bc26b77c349d9aac0b1
Saranya <79350298+saranyamohank@users.noreply.github.com>
2023-05-16 11:33:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request
+import pandas as pd
+from joblib import load
+from flask_cors import CORS
 
+# load the trained model and vectorizer
+clf = load('model.joblib')
+#vectorizer = TfidfVectorizer()
+vectorizer = load('vectorizer.joblib')
+
+# create the Flask app
 app = Flask(__name__)
+label_dict = ["Fake", "Not Fake"]
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# define the endpoint
+@app.route('/text', methods=['POST'])
+def predict():
+    text = request.form['text']
+    text_tfidf = vectorizer.transform([text])
+    # make a prediction using the trained classifier
+    prediction = clf.predict(text_tfidf)
+    # return the prediction as a JSON response
+    return {'prediction': label_dict[prediction[0]]}
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+@app.route('/data', methods=['POST'])
+def predict_csv():
+    file = request.files['file']
+    df = pd.read_csv(file)
+    text_tfidf = vectorizer.transform(df['text'])
+    predictions = clf.predict(text_tfidf)
+    df['prediction'] = [label_dict[prediction] for prediction in predictions]
+    results = []
+    for index, row in df.iterrows():
+        results.append({
+            'text': row['text'],
+            'prediction': row['prediction']
+        })
+    return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True) 
