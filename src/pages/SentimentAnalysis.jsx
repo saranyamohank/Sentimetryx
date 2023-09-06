d9eb504ff53d46360423a41e5c94ee74b62a7532
bharathi-rmps <79350260+bharathi-rmps@users.noreply.github.com>
2023-09-06 13:20:20 +0530

    Update SentimentAnalysis.jsx

--- src/pages/SentimentAnalysis.jsx
+++ src/pages/SentimentAnalysis.jsx
@@ -33,7 +33,7 @@ const SentimentAnalysis = () => {
     setIsLoading(true);
 
     axios
-      .post('https://35.89.167.107:5173/text_sentiment', { text })
+      .post('http://127.0.0.1:4040/text_sentiment', { text })
       .then((response) => {
         console.log(response.data);
         setResult(
@@ -67,7 +67,7 @@ const SentimentAnalysis = () => {
 
     if (isAudio) {
       axios
-        .post('http://localhost:5000/audio_sentiment', formData, {
+        .post('http://127.0.0.1:4040/audio_sentiment', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
@@ -91,7 +91,7 @@ const SentimentAnalysis = () => {
         });
     } else {
       axios
-        .post('http://localhost:5000/file_sentiment', formData, {
+        .post('http://127.0.0.1:4040/file_sentiment', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
